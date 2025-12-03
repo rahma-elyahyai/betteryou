8e94c0e19909415c10b54352f89e1458f9e74cfb
rahma-elyahyai <rahmaelyahyai6@gmail.com>
2025-12-03 23:28:10 +0100

    initial push

---
+++ backend/src/test/java/ma/betteryou/betteryou_backend/BetteryouBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.betteryou.betteryou_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetteryouBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
