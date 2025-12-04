400343e391ea0d3a782d51ee979af75dbb846b2b
rahma-elyahyai <rahmaelyahyai6@gmail.com>
2025-12-04 01:34:46 +0100

    fix backend mvnw permissions in C.I

--- backend/src/test/java/ma/betteryou/betteryoubackend/BetteryouBackendApplicationTests.java
+++ backend/src/test/java/ma/betteryou/betteryoubackend/BetteryouBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package ma.betteryou.betteryou_backend;
+package ma.betteryou.betteryoubackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BetteryouBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
