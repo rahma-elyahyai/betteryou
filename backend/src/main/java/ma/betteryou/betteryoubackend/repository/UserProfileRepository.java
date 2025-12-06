b9911f876337d2a82f73c1f3e7f71ef5fe2742e8
manar-ouahabi <manarouahabi13@gmail.com>
2025-12-06 17:05:41 +0100

    Remove deprecated UserProfile feature and duplicate application file

--- backend/src/main/java/ma/betteryou/betteryoubackend/repository/UserProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.betteryou.betteryoubackend.repository;
-
-import ma.betteryou.betteryoubackend.entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
