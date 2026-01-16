a7b2d4b28b5a592094ed82775180a59c883f599f
khadija11-ws <khadijaouassi36@gmail.com>
2026-01-16 20:37:01 +0100

    conflit resolu

--- backend/src/main/java/ma/betteryou/betteryoubackend/repository/UserRepository.java
+++ backend/src/main/java/ma/betteryou/betteryoubackend/repository/UserRepository.java
@@ -4,11 +4,8 @@ import ma.betteryou.betteryoubackend.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> { //
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { // 🔴 Long ici
-
+public interface UserRepository extends JpaRepository<User, Long> { //
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
