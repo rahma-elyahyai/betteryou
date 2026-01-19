cc313f62bdcbe8cf6ce760945dccb49d66c370b3
SaraLrs80 <saralaaroussi8@gmail.com>
2026-01-19 22:47:50 +0100

    Fix VITE API baseURL et mise en place de deploiment auto et correction docker compose

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 
-// ✅ Axios instance EXPORTÉE
-export const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
+/**
+ * Vite: variables injectées au build via import.meta.env
+ * Fallback:
+ * - en local: localhost:8080
+ * - en prod: même hostname que le site + port 8080 (ex: http://92.5.238.53:8080)
+ */
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  (window.location.hostname === "localhost"
+    ? "http://localhost:8080"
+    : `${window.location.protocol}//${window.location.hostname}:8080`);
+
+export const api = axios.create({ baseURL });
+
 
 // ✅ Interceptor → ajoute le token automatiquement
 api.interceptors.request.use(
