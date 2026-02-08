c22fa65e711c988a726b06d24cc63ef512e042e5
rahma-elyahyai <rahmaelyahyai6@gmail.com>
2026-02-08 22:21:56 +0000

    supp data.sql

--- backend/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO workouts (title, target_muscles, duration, level, image_url) VALUES
-                                                                             ('Bench Press', 'Pectorals, Triceps, Anterior Deltoids', '20-25 min', 'Intermediate',
-                                                                              'https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg'),
-                                                                             ('Squats', 'Quadriceps, Glutes, Hamstrings', '15-20 min', 'Beginner',
-                                                                              'https://images.pexels.com/photos/2261485/pexels-photo-2261485.jpeg'),
-                                                                             ('Burpees', 'Full Body', '10-15 min', 'Advanced',
-                                                                              'https://images.pexels.com/photos/1552103/pexels-photo-1552103.jpeg'),
-                                                                             ('Pull-Ups', 'Lats, Biceps, Forearms', '15-20 min', 'Intermediate',
-                                                                              'https://images.pexels.com/photos/3837781/pexels-photo-3837781.jpeg');
