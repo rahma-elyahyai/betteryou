7b2d023f63a24506c330821e2229dbd1f0f4af83
rahma-elyahyai <rahmaelyahyai6@gmail.com>
2026-02-10 12:32:22 +0000

    corr de api

--- frontend/src/api/Workout/workoutApi.js
+++ frontend/src/api/Workout/workoutApi.js
@@ -1,14 +1,12 @@
 
 import { api } from "../auth";
 
-
-
 export const fetchWorkouts = async () => {
-  const response = api.get(`/api/workouts`);
+  const response = await api.get(`/api/workouts`);
   return response.data;
 };
 
 export const fetchWorkoutDetail = async (id) => {
-  const response = api.get(`/api/workouts/${id}/detail`);
+  const response = await api.get(`/api/workouts/${id}/detail`);
   return response.data;
 };
